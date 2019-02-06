7c36efa34149762994dbf6532ec87ecc18ece5b6
Sheru Aravind Reddy <sheru.reddy@iitgn.ac.in>
2019-02-06 11:15:43 +0530

    Update regex1.py

--- Sample_Codes/regex1.py
+++ Sample_Codes/regex1.py
@@ -1,7 +1,5 @@
 import re
-import nltk
 import pandas as pd
-import matplotlib.pyplot as plt
 
 tweet = "model take all time friday smile user cookie people use cons con personalised booked"
 
@@ -19,28 +17,3 @@ tweet = func("@[\w]*", tweet, '')
 r = re.findall("[^A-Za-z]", tweet)
 for i in r:
     tweet = tweet.replace(i, " ")    
-
-#...Removing preposiitons, conjuctions and pronouns from the tweet
-words = nltk.word_tokenize(tweet)
-tags = nltk.pos_tag(words)
-print tags
-#Tags_list = pd.read_html('http://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html')[0][1]
-del_tags = ['PRP','DT','CC', 'IN', 'PRP$', 'VBP', 'NNP', 'VBZ', 'VB', 'MD', 'RB', 'VBD', 'WP', 'CD', 'WRB', 'WDT']
-
-new_tags = []
-for i in tags:
-    if i[1] not in del_tags:
-        new_tags.append(i[0])
-
-tweet = " ".join(new_tags)
-print tweet
-
-# =============================================================================
-# from wordcloud import WordCloud
-# wordcloud = WordCloud(width = 800, height = 500, random_state = 21, max_font_size = 110).generate(tweet)
-# 
-# plt.figure(figsize = (10,7))
-# plt.imshow(wordcloud, interpolation = 'bilinear')
-# plt.axis('off')
-# plt.show()
-# =============================================================================
